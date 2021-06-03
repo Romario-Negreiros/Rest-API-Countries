f52d6c84413230aae5b3ae9ece0fa91688e7597a
Romario-Negreiros <nromario482@gmail.com>
2021-06-03 08:33:15 -0300

    dark mode fixed

--- detailpage/detailPage.js
+++ detailpage/detailPage.js
@@ -4,4 +4,9 @@ swapTheme.addEventListener('click', () => {
     document.querySelector('.a-header').classList.toggle('a-header--active')
     document.querySelector('.darkMode').classList.toggle('moon-inative')
     document.querySelector('.lightMode').classList.toggle('sun-active')
+    const [...spans] = document.querySelectorAll('.borderCountries')
+    spans.forEach(v => {
+        v.classList.toggle('borderCountries--active')
+    })
+    document.querySelector('.b-country a').classList.toggle('back--active')
 })
